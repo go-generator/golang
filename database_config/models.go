241aad41b9ced2689937d548f056622ed6f79b30
Nhut Thanh <nhutthanh1096@@gmail.com>
2020-01-09 11:23:47 +0700

    + Add type convert file, add relationship many to one field

--- database_config/models.go
+++ database_config/models.go
@@ -1,16 +1,11 @@
 package database_config
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
-	"os"
 	"strconv"
 	"strings"
 
 	"fyne.io/fyne"
-	fApp "fyne.io/fyne/app"
-	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mssql"
@@ -18,7 +13,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/pkg/errors"
-	"github.com/sqweek/dialog"
+	"github.com/spf13/viper"
 )
 
 type DatabaseConfig struct {
@@ -111,88 +106,6 @@ func (dc *DatabaseConfig) SetDatabaseName(value string) {
 	dc.Database = value
 }
 
-func (dc *DatabaseConfig) InputValidation(app fyne.App) {
-	if dc.Dialect == "" {
-		ShowWindows(app, "Error", "Invalid Dialect")
-		return
-	}
-	if dc.User == "" {
-		ShowWindows(app, "Error", "Invalid User")
-		return
-	}
-	if dc.Password == "" {
-		ShowWindows(app, "Error", "Invalid Password")
-		return
-	}
-	if dc.Host == "" {
-		ShowWindows(app, "Error", "Invalid Host Address")
-		return
-	}
-	if _, err := strconv.Atoi(strconv.Itoa(dc.Port)); err != nil {
-		ShowWindows(app, "Error", "Invalid Port")
-		return
-	}
-	if dc.Database == "" {
-		ShowWindows(app, "Error", "Invalid Database Name")
-		return
-	}
-	filename, errFile := dialog.File().Filter("JSON files", "json").Title("Save As").Save()
-	if errFile != nil {
-		ShowWindows(app, "Error", errFile.Error())
-		return
-	}
-	tokens := strings.Split(filename, string(os.PathSeparator))
-	err := dc.JsonUI(tokens[len(tokens)-2], filename+".json")
-	if err != nil {
-		ShowWindows(app, "Error", err.Error())
-		return
-	}
-	ShowWindows(app, "Success", "Generated Database Json Description Successfully")
-}
-
-func (dc *DatabaseConfig) InputUI() fyne.Window {
-	app := fApp.New()
-	app.Settings().SetTheme(theme.LightTheme())
-	w := app.NewWindow("Database Json Generator")
-	w.Resize(fyne.Size{
-		Width: 640,
-	})
-	dialectEntry := widget.NewEntry()
-	dialectEntry.OnChanged = dc.SetDialect
-	usernameEntry := widget.NewEntry()
-	usernameEntry.OnChanged = dc.SetUsername
-	passwordEntry := widget.NewEntry()
-	passwordEntry.OnChanged = dc.SetPassword
-	passwordEntry.Password = true
-	hostEntry := widget.NewEntry()
-	hostEntry.OnChanged = dc.SetHost
-	portEntry := widget.NewEntry()
-	portEntry.OnChanged = dc.SetPort
-	databaseEntry := widget.NewEntry()
-	databaseEntry.OnChanged = dc.SetDatabaseName
-	w.SetContent(widget.NewVBox(
-		widget.NewLabel("Dialect:"),
-		dialectEntry,
-		widget.NewLabel("User:"),
-		usernameEntry,
-		widget.NewLabel("Password:"),
-		passwordEntry,
-		widget.NewLabel("Host:"),
-		hostEntry,
-		widget.NewLabel("Port:"),
-		portEntry,
-		widget.NewLabel("Database:"),
-		databaseEntry,
-		widget.NewButton("Generate Database Json Description", func() {
-			dc.InputValidation(app)
-		}),
-		widget.NewButton("Quit", func() {
-			app.Quit()
-		}),
-	))
-	return w
-}
-
 func ShowWindows(app fyne.App, title, message string) {
 	wa := app.NewWindow(title)
 	wa.Resize(fyne.Size{
@@ -204,67 +117,12 @@ func ShowWindows(app fyne.App, title, message string) {
 	wa.Show()
 }
 
-func (dc *DatabaseConfig) JsonUI(env, filePath string) error { //s *SqlTablesData, conn *gorm.DB, tables []string, packageName, output string) {
-	var s SqlTablesData
-	var files FilesDetails
-	conn, err := dc.ConnectToSqlServer()
-	log.Println("Connecting to sql server...")
-	if err != nil {
-		return err
-	}
-	log.Println("Connection to sql server is established successfully")
-	tables := ListAllTableNames(conn, dc.Database)
-	defer s.FreeResources() // Close connection before freeing resources
-	defer func() {
-		err = conn.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-	s.InitSqlTablesData()
-	files.Env = env
-	for _, v := range tables {
-		var m ModelJSON
-		m.Name = v
-		s.InitSqlTable(v, conn)
-		s.StandardizeFieldsName()
-		for i, v := range s.SqlTable {
-			var f FieldElements
-			if s.ContainCompositeKey {
-				f.Source = ToLower(s.GoFields[i])
-			} else {
-				if v.ColumnKey == "PRI" {
-					f.Source = "_id"
-				} else {
-					f.Source = ToLower(s.GoFields[i])
-				}
-			}
-			f.Name = s.GoFields[i]
-			f.Type = s.TypeMap[v.DataType]
-			if v.ColumnKey == "PRI" {
-				f.PrimaryKey = true
-			} else {
-				f.PrimaryKey = false
-			}
-			m.Fields = append(m.Fields, f)
-		}
-		files.Files = append(files.Files, m)
-		s.ResetData() // Reuse Variable
-	}
-	data, err := json.MarshalIndent(&files, "", " ")
-	if err != nil {
-		return err
-	}
-	err = ioutil.WriteFile(filePath, data, 0644) // Create and write files
-	if err != nil {
-		return err
-	}
-	return err
+type TypeMap struct {
+	TypeConvert map[string]string `yaml:"typeConvert"`
 }
 
-type TypeMap map[string]string
-
 type SqlTable struct {
+	TableName              string `gorm:"column:TABLE_NAME"`
 	ColumnName             string `gorm:"column:COLUMN_NAME"`
 	DataType               string `gorm:"column:DATA_TYPE"`
 	IsNullable             string `gorm:"column:IS_NULLABLE"`
@@ -308,16 +166,18 @@ func (s *SqlTablesData) ResetData() {
 }
 
 func (s *SqlTablesData) InitTypeMap() {
-	s.TypeMap = make(TypeMap)
-	s.TypeMap["decimal"] = "int"
-	s.TypeMap["char"] = "string"
-	s.TypeMap["varchar"] = "string"
-	s.TypeMap["int"] = "int"
-	s.TypeMap["timestamp"] = "time.Time"
-	s.TypeMap["date"] = "time.Time"
-	s.TypeMap["text"] = "string"
-	s.TypeMap["smallint"] = "int8"
-	s.TypeMap["bigint"] = "int64"
+	viper.SetConfigName("data_type")
+	viper.AddConfigPath("../data_type")
+	viper.AutomaticEnv()
+	viper.SetConfigType("yml")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Println("Error while reading config file, " + err.Error())
+	}
+	err := viper.Unmarshal(&s.TypeMap)
+	log.Println(s.TypeMap.TypeConvert)
+	if err != nil {
+		log.Println("Error while unmarshal file, " + err.Error())
+	}
 }
 
 func (s *SqlTablesData) InitSqlTable(tableName string, conn *gorm.DB) {
@@ -326,8 +186,8 @@ func (s *SqlTablesData) InitSqlTable(tableName string, conn *gorm.DB) {
 }
 
 func (s *SqlTablesData) FreeResources() {
-	for k, _ := range s.TypeMap {
-		delete(s.TypeMap, k)
+	for k, _ := range s.TypeMap.TypeConvert {
+		delete(s.TypeMap.TypeConvert, k)
 	}
 	log.Println("Resources are freed successfully")
 }
@@ -348,18 +208,18 @@ func (s *SqlTablesData) WriteStruct() {
 	if !s.ContainCompositeKey { // Only one Primary key
 		for i, v := range s.GoFields {
 			if s.SqlTable[i].ColumnKey == "PRI" {
-				s.WriteFile.WriteString("\t" + AddStructFieldName(v) + "\t" + s.TypeMap[s.SqlTable[i].DataType] + "\t" + AddJSONTag(ToLower(v)) + AddBSONTag("_id") + AddGORMTag(s.SqlTable[i].ColumnName, true))
+				s.WriteFile.WriteString("\t" + AddStructFieldName(v) + "\t" + s.TypeMap.TypeConvert[s.SqlTable[i].DataType] + "\t" + AddJSONTag(ToLower(v)) + AddBSONTag("_id") + AddGORMTag(s.SqlTable[i].ColumnName, true))
 				continue
 			}
-			s.WriteFile.WriteString("\t" + AddStructFieldName(v) + "\t" + s.TypeMap[s.SqlTable[i].DataType] + "\t" + AddJSONTag(ToLower(v)) + AddBSONTag(ToLower(v)) + AddGORMTag(s.SqlTable[i].ColumnName, false))
+			s.WriteFile.WriteString("\t" + AddStructFieldName(v) + "\t" + s.TypeMap.TypeConvert[s.SqlTable[i].DataType] + "\t" + AddJSONTag(ToLower(v)) + AddBSONTag(ToLower(v)) + AddGORMTag(s.SqlTable[i].ColumnName, false))
 		}
 	} else { // Composite key
 		for i, v := range s.GoFields {
 			if s.SqlTable[i].ColumnKey == "PRI" {
-				s.WriteFile.WriteString("\t" + AddStructFieldName(v) + "\t" + s.TypeMap[s.SqlTable[i].DataType] + "\t" + AddJSONTag(ToLower(v)) + AddBSONTag(ToLower(v)) + AddGORMTag(s.SqlTable[i].ColumnName, true))
+				s.WriteFile.WriteString("\t" + AddStructFieldName(v) + "\t" + s.TypeMap.TypeConvert[s.SqlTable[i].DataType] + "\t" + AddJSONTag(ToLower(v)) + AddBSONTag(ToLower(v)) + AddGORMTag(s.SqlTable[i].ColumnName, true))
 				continue
 			}
-			s.WriteFile.WriteString("\t" + AddStructFieldName(v) + "\t" + s.TypeMap[s.SqlTable[i].DataType] + "\t" + AddJSONTag(ToLower(v)) + AddBSONTag(ToLower(v)) + AddGORMTag(s.SqlTable[i].ColumnName, false))
+			s.WriteFile.WriteString("\t" + AddStructFieldName(v) + "\t" + s.TypeMap.TypeConvert[s.SqlTable[i].DataType] + "\t" + AddJSONTag(ToLower(v)) + AddBSONTag(ToLower(v)) + AddGORMTag(s.SqlTable[i].ColumnName, false))
 		}
 	}
 	s.WriteFile.WriteString("}")
@@ -368,61 +228,4 @@ func (s *SqlTablesData) WriteStruct() {
 func (s *SqlTablesData) CreateContent(packageName string) {
 	s.WritePackage(packageName)
 	s.WriteStruct()
-}
-
-func (dc *DatabaseConfig) JsonDescriptionGenerator(env, output string, conn *gorm.DB) error { //s *SqlTablesData, conn *gorm.DB, tables []string, packageName, output string) {
-	var s SqlTablesData
-	var files FilesDetails
-	if env == "" {
-		env = "database_description"
-	}
-	tables := ListAllTableNames(conn, dc.Database)
-	defer s.FreeResources() // Close connection before freeing resources
-	files.Env = env
-	s.InitSqlTablesData()
-	path := "./" + output + "/"
-	fileDirectory := path + output + ".json"
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		err = os.Mkdir(path, 0777)
-		if err != nil {
-			return err
-		}
-	}
-	for _, v := range tables {
-		var m ModelJSON
-		m.Name = v
-		s.InitSqlTable(v, conn)
-		s.StandardizeFieldsName()
-		for i, v := range s.SqlTable {
-			var f FieldElements
-			if s.ContainCompositeKey {
-				f.Source = ToLower(s.GoFields[i])
-			} else {
-				if v.ColumnKey == "PRI" {
-					f.Source = "_id"
-				} else {
-					f.Source = ToLower(s.GoFields[i])
-				}
-			}
-			f.Name = s.GoFields[i]
-			f.Type = s.TypeMap[v.DataType]
-			if v.ColumnKey == "PRI" {
-				f.PrimaryKey = true
-			} else {
-				f.PrimaryKey = false
-			}
-			m.Fields = append(m.Fields, f)
-		}
-		files.Files = append(files.Files, m)
-		s.ResetData() // Reuse Variable
-	}
-	data, err := json.MarshalIndent(&files, "", " ")
-	if err != nil {
-		return err
-	}
-	err = ioutil.WriteFile(fileDirectory, data, 0644) // Create and write files
-	if err != nil {
-		return err
-	}
-	return err
 }
